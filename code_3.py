edb467bd102f8b02316e6a5019501590bedde53c
Simone <144659113+Simonesho@users.noreply.github.com>
2024-01-19 21:40:54 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,29 @@
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+function main() {
+    data.forEach(item => {
+    return data;
+
+    for (let i = 0; i < 10; i++) {
+    });
+}
+        console.log("Random Number: " + item);
+    }
+    const data = [];
+function generateRandomData() {
+    const data = generateRandomData();
+main();
+
+    }
+    public static void main(String[] args) {
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
+
+        }
+}
+        int[] data = new int[10];
+        }
+        for (int item : data) {
